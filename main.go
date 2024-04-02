754009023480e8cec2c86edfc0a7c05865368a7b
Matt <mattfields+dev@pm.me>
2024-04-02 18:36:09 +1000

    add main.go and placeholders for types

---
+++ main.go
@@ -0,0 +1,15 @@
+// We'll initially keep all project logic here, until it grows to the point we should split it out.
+package main
+
+// Just add placeholders to start...
+// Game represents an entire chess game, with move history, board, players, and game state
+type Game struct{}
+
+// Board represents a chess board, with pieces and positions
+type Board struct{}
+
+// Piece represents a chess piece
+type Piece interface{}
+
+// Player is a single player in the chess game
+type Player struct{}
